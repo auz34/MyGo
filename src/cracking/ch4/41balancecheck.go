4da61049413e0a9aa3200fc94ae9be8e528c5263
auz34 <andrewzavgorodniy@gmail.com>
2016-04-05 22:34:07 -0700

    check whether tree is balanced completed

--- src/cracking/ch4/41balancecheck.go
+++ src/cracking/ch4/41balancecheck.go
@@ -5,9 +5,28 @@ package main
 
 import (
 	"fmt"
+	"math"
+	"cracking/ch4/basics"
 )
 
+func isBalanced(tree *basics.TreeNode) (bool, int) {
+	if tree == nil {
+		return true, 0
+	}
+
+	b1, h1 := isBalanced(tree.Left)
+	b2, h2 := isBalanced(tree.Right)
+
+	b := b1 && b2 && (math.Abs(float64(h1-h2)) < 2)
+	h :=  1 + int(math.Max(float64(h1), float64(h2)))
+
+	return b, h
+}
+
 func main() {
-	fmt.Print("Haven't started yet")
+	tree := basics.GenerateNonBalancedTree()
+
+	balanced, _ := isBalanced(tree)
+	fmt.Printf("Tree is balanced: %v \n", balanced)
 }
 
