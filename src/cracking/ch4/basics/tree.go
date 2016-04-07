0339f9a55d7beaa6622a4c15b542af4adc3fbee9
auz34 <andrewzavgorodniy@gmail.com>
2016-04-07 16:40:52 -0700

    print tree func

--- src/cracking/ch4/basics/tree.go
+++ src/cracking/ch4/basics/tree.go
@@ -1,5 +1,7 @@
 package basics
 
+import "fmt"
+
 type TreeNode struct {
 	Value int
 	Left *TreeNode
@@ -25,4 +27,36 @@ func GenerateNonBalancedTree() *TreeNode {
 
 
 	return root
+}
+
+func traverseAndCollect(node *TreeNode, level int, results map[int]string) {
+	if (node == nil) {
+		return
+	}
+
+	if _, ok := results[level]; !ok {
+		results[level] = ""
+	}
+
+	left := "nil"
+	if node.Left != nil {
+		left = fmt.Sprintf("%v", node.Left.Value)
+	}
+
+	right := "nil"
+	if node.Right != nil {
+		right = fmt.Sprintf("%v", node.Right.Value)
+	}
+	results[level] += fmt.Sprintf(" %v (l:%v; r:%v)  ", node.Value, left, right)
+	traverseAndCollect(node.Left, level + 1, results)
+	traverseAndCollect(node.Right, level + 1, results)
+}
+
+func PrintTreeByLevels(tree *TreeNode) {
+	results := make(map[int]string)
+	traverseAndCollect(tree, 0, results)
+
+	for level, value := range results {
+		fmt.Printf("%v: %s \n", level, value)
+	}
 }
