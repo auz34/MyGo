abc22c12321a19ca7a386ed2fd95e6b1a794bc5c
auz34 <andrewzavgorodniy@gmail.com>
2016-04-08 15:49:54 -0700

    improved solution to visit each tree node only once

--- src/cracking/ch4/48treepath.go
+++ src/cracking/ch4/48treepath.go
@@ -11,25 +11,37 @@ import (
 func main() {
 	tree := basics.GenerateNonBalancedTree2()
 	fmt.Printf("Pathes for 26: \n")
-	findPath(tree, 26, 26, "")
+	findPath(tree, 26, make([]candidate, 0))
 }
 
-func findPath(tree *basics.TreeNode, total, sumLeft int, path string) {
+type candidate struct {
+	path string
+	sumLeft int
+}
+
+func findPath(tree *basics.TreeNode, total int, candidates []candidate) {
 	if tree == nil {
 		return
 	}
 
-	curpath := fmt.Sprintf("%v -> %v", path, tree.Value)
-	if tree.Value == sumLeft {
-		fmt.Printf("Solution: %v \n", curpath)
+	if tree.Value == total {
+		fmt.Printf("Single element solution: %v \n", tree.Value)
 	}
 
-	sumLeft = sumLeft - tree.Value
-	if sumLeft > 0 {
-		findPath(tree.Left, total, sumLeft, curpath)
-		findPath(tree.Right, total, sumLeft, curpath)
+	newCandidates := make([]candidate, 0)
+	for _, c := range candidates {
+		if c.sumLeft == tree.Value {
+			fmt.Printf("Solution: %v -> %v \n", c.path, tree.Value)
+		} else if c.sumLeft > tree.Value {
+			newPartialPath := fmt.Sprintf("%v -> %v", c.path, tree.Value)
+			newSumLeft := c.sumLeft - tree.Value
+			newCand := candidate{newPartialPath, newSumLeft}
+			newCandidates = append(newCandidates, newCand)
+		}
 	}
 
-	findPath(tree.Left, total, total, "")
-	findPath(tree.Right, total, total, "")
+	newCandidates = append(newCandidates, candidate{fmt.Sprintf("%v", tree.Value), total - tree.Value})
+
+	findPath(tree.Left, total, newCandidates)
+	findPath(tree.Right, total, newCandidates)
 }
