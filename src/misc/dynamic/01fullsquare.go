e4784a3a4d17037f152bed8c2725612fcd642f0b
auz34 <andrewzavgorodniy@gmail.com>
2016-04-09 00:36:10 -0700

    tasks for future

---
+++ src/misc/dynamic/01fullsquare.go
@@ -0,0 +1,29 @@
+//2. Given a 2D matrix of booleans, find the biggest continuous square submatrix of "trues" and return its dimension.
+//Example:
+// t t t t t t
+// t t t f f f
+// t t t f f f
+// t f f t t f returns 3
+
+// t t f t
+// f t f t
+// t t t t
+// f t t f returns 2
+
+// f f
+// t t returns 1
+
+// f f f
+// f f f
+// f f f returns 0
+
+
+// 3. The problem:
+// There are a row of houses. Each house can be painted with three colors: red, blue and green.
+// The cost of painting each house with a certain color is different. You have to paint all the houses such that no two adjacent houses have the same color.
+// You have to paint the houses with minimum cost. How would you do it?
+// Note: The cost of painting house 1 red is different from that of painting house 2 red. Each combination of house and color has its own cost.
+
+//Example:
+//3 houses:
+//r = [1,4,6], g = [2,100,2], b = [3,100,4]
