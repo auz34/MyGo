13ca83dba704c299df7dac1e4702caa6cef6e590
auz34 <andrewzavgorodniy@gmail.com>
2016-04-10 14:33:16 -0700

    multiply to ints without using * operator

--- src/cracking/ch8/85RecursiveMultiply.go
+++ src/cracking/ch8/85RecursiveMultiply.go
@@ -1,2 +1,26 @@
 // Write a recursive function to multiply two positive integers without using * operator.
 // You canuse addition, subtraction and bit shifting, but you should minimize the number of those operations.
+package main
+
+import "fmt"
+
+func main() {
+	a := uint(25)
+	b := uint(17)
+	fmt.Printf("%v*%v=%v\n", a, b, multiply(a,b))
+}
+
+func multiply(a, b uint) uint {
+	if a == 0 || b == 0 {
+		return 0
+	}
+
+	for b & uint(1) == 0 {
+		a = a << 1
+		b = b >> 1
+	}
+
+	sum := a
+	b -= 1
+	return sum + multiply(a, b)
+}
