86f04c36510b9d5cfaa06062888c0c2296be0fe5
auz34 <andrewzavgorodniy@gmail.com>
2016-04-10 13:40:27 -0700

    find magic index in sorted array

--- src/cracking/ch8/83magicindex.go
+++ src/cracking/ch8/83magicindex.go
@@ -2,3 +2,78 @@
 // of distinct integers, write a method to find a magic index, if one exists, in array A.
 // FOLLOW UP
 // What if the values are not distinct?
+package main
+
+import "fmt"
+
+func main() {
+	ar1 := []int {-10, -2, 1, 3, 6, 12, 15, 17, 18 }
+	ar2 := []int {-10, -2, -1, 1, 3, 4, 5, 7, 18 }
+	ar3 := []int {-10, 2, 2, 2, 2, 4, 5, 9, 18 }
+
+
+	fmt.Printf("Brute force answers: %v, %v, %v \n", bruteForce(ar1), bruteForce(ar2), bruteForce(ar3))
+
+	fmt.Printf("Answer for first array: %v \n", findDistinct(ar1, 0, len(ar1) - 1))
+	fmt.Printf("Answer for second array: %v \n", findDistinct(ar2, 0, len(ar2) - 1))
+
+	fmt.Printf("Answer for third array: %v \n", findNonDistinct(ar3, 0, len(ar3) - 1))
+}
+
+func bruteForce(array []int) int {
+	for i:=0; i<len(array); i++ {
+		if array[i]==i {
+			return i
+		}
+	}
+
+	return -1
+}
+
+func findDistinct(array []int, left, right int) int {
+	if left>right {
+		return -1
+	}
+
+	mid := (left + right) / 2
+
+	if mid == array[mid] {
+		return mid
+	}
+
+	if mid < array[mid] {
+		return findDistinct(array, left, mid - 1)
+	}
+
+	return findDistinct(array, mid+1, right)
+}
+
+func findNonDistinct(array []int, left, right int) int {
+	if left>right {
+		return -1
+	}
+
+	mid := (left + right) / 2
+
+	if mid == array[mid] {
+		return mid
+	}
+
+	if mid < array[mid] {
+		diff := array[mid] - mid
+		lr := findNonDistinct(array, left, mid - 1)
+		if lr == -1 {
+			return findNonDistinct(array, mid + diff, right)
+		} else {
+			return lr
+		}
+	} else {
+		diff := mid - array[mid]
+		rr := findNonDistinct(array, mid+1, right)
+		if rr == -1 {
+			return findNonDistinct(array, left, mid - diff)
+		} else {
+			return rr
+		}
+	}
+}
