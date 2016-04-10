447132ddcfaa5e840750e2015f0b6521127fab9e
auz34 <andrewzavgorodniy@gmail.com>
2016-04-10 13:55:10 -0700

    find all subsets

--- src/cracking/ch8/84PowerSet.go
+++ src/cracking/ch8/84PowerSet.go
@@ -1,1 +1,22 @@
 // Write a method to return all subsets of a set
+
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	set := "abcd"
+	printSubsets("", set)
+}
+
+func printSubsets(curSet, leftItems string) {
+	fmt.Printf("Subset: {%v}\n", curSet)
+	for i:=0; i < len(leftItems); {
+		r, size := utf8.DecodeRuneInString(leftItems[i:])
+		printSubsets(curSet+string(r), leftItems[i+size:])
+		i += size
+	}
+}
