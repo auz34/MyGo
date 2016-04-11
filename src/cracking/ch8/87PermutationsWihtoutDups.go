96b446034f06208fa673e92d6c84be8e8eaa5241
auz34 <andrewzavgorodniy@gmail.com>
2016-04-10 19:59:37 -0700

    find all permutations of a string

--- src/cracking/ch8/87PermutationsWihtoutDups.go
+++ src/cracking/ch8/87PermutationsWihtoutDups.go
@@ -1,1 +1,28 @@
-// Write a method to compute all permutatioms of a string of unique characters
+// Write a method to compute all permutations of a string of unique characters
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "abcd"
+	printPermutations(s, "")
+}
+
+func printPermutations(input, prefix string) {
+	l := len(input)
+	if (l == 0) {
+		fmt.Printf("Permutation: %v \n", prefix)
+	}
+
+	for i:=0; i<l; {
+		r, w := utf8.DecodeRuneInString(input[i:])
+		nextI := input[0:i] + input[i+w:]
+		nextP := prefix + string(r)
+		printPermutations(nextI, nextP)
+		i += w
+	}
+}
+
