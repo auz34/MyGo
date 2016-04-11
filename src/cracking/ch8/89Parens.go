191df58041f19892a24df1259af58713a86c7a9a
auz34 <andrewzavgorodniy@gmail.com>
2016-04-10 20:10:30 -0700

    find all valid parentheses combinations

--- src/cracking/ch8/89Parens.go
+++ src/cracking/ch8/89Parens.go
@@ -1,4 +1,33 @@
-/*Implement an algorithm to print all valid (e.g., properly opened and closed) combi- nations of n-pairs of parentheses.
+/*Implement an algorithm to print all valid (e.g., properly opened and closed) combinations of n-pairs of parentheses.
 EXAMPLE:
 input: 3 (e.g., 3 pairs of parentheses)
 output: ()()(), ()(()), (())(), ((()))*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Print("For 3 pairs following solutions are possible: \n")
+	printParens(3, 3, "")
+}
+
+func printParens(l, r int, prefix string) {
+	if l == 0 && r == 0 {
+		fmt.Printf("Solution: %v \n", prefix)
+		return
+	}
+
+	if l==0 {
+		printParens(l, r-1, prefix + ")")
+		return
+	}
+
+	if (l < r) {
+		printParens(l-1, r, prefix + "(")
+		printParens(l, r-1, prefix + ")")
+		return
+	}
+
+	printParens(l-1, r, prefix + "(")
+}
