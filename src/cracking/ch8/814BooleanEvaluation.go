b561686b621998fb7bf9598a33435e2fc16e99c4
auz34 <andrewzavgorodniy@gmail.com>
2016-04-12 19:09:05 -0700

    start with boolean evaluation task

--- src/cracking/ch8/814BooleanEvaluation.go
+++ src/cracking/ch8/814BooleanEvaluation.go
@@ -4,3 +4,47 @@
 // EXAMPLE
 // countEval("1^0|0|1", false) -> 2
 // countEval("0$0$0$1^1|1", true) -> 10
+
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	fmt.Printf("((1^0)|0)|1 = %v", eval("((1^0)|0)|1"))
+}
+
+func translateToBool(r rune) bool {
+	if r == "0" {
+		return false
+	} else {
+		return true
+	}
+}
+
+func evalSimpleExpr(expr string) bool {
+	// first rune is always 1 or 0
+	r, _ := utf8.DecodeRuneInString(expr[0:])
+	result := translateToBool(r)
+
+	for i:=1; i<len(expr); i+=2 {
+		opr, _ := utf8.DecodeRuneInString(expr[i:])
+		r2, _ := utf8.DecodeRuneInString(expr[i+1:])
+		switch opr {
+		case "&":
+			result = result && translateToBool(r2)
+		case "|":
+			result = result || translateToBool(r2)
+		case "^":
+			result = result ^ translateToBool(r2)
+		}
+	}
+
+	return result
+}
+
+func eval(expr string) bool {
+	return evalSimpleExpr(expr)
+}
