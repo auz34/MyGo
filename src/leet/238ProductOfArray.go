e34acce9ed1678e9cc4ba0de0479e0068f04c454
auz34 <andrewzavgorodniy@gmail.com>
2016-05-08 10:37:59 -0700

    first solution for product of array problem

---
+++ src/leet/238ProductOfArray.go
@@ -0,0 +1,61 @@
+/*
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of
+all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the
+purpose of space complexity analysis.)
+*/
+package main
+
+import "fmt"
+
+func main() {
+	ar := []int {1,2,3,4}
+
+	fmt.Printf("Result: %v", productExceptSelf(ar))
+}
+
+func productExceptSelf(nums []int) []int {
+	fw := make([]int, len(nums))
+	bk := make([]int, len(nums))
+
+	for i:=0; i< len(nums); i++ {
+		prev := 1
+		if i>0 {
+			prev = fw[i-1]
+		}
+
+		fw[i] = prev*nums[i]
+	}
+
+	for i:=len(nums) - 1; i>0; i-- {
+		prev := 1
+		if i<len(nums) - 1 {
+			prev = bk[i+1]
+		}
+
+		bk[i] = prev*nums[i]
+	}
+
+	result := make([]int, len(nums))
+	for i:=0; i< len(nums); i++ {
+		prev := 1
+		if i>0 {
+			prev = fw[i-1]
+		}
+
+		next := 1
+		if i<len(nums) - 1 {
+			next = bk[i+1]
+		}
+
+		result[i] = prev*next
+	}
+
+	return result
+}
