2ce3b39ef94faa04cc0328c3df828ad59355f926
auz34 <andrewzavgorodniy@gmail.com>
2016-05-10 16:40:08 -0700

    improvent to tak into consideration case with duplicates in t

--- src/leet/76MinWindowsSubstring.cs
+++ src/leet/76MinWindowsSubstring.cs
@@ -15,33 +15,102 @@ namespace MinWindowSubstring
             var solution = new Solution();
             Console.WriteLine("Use Case #1: {0}", solution.MinWindow(s, t));
             Console.WriteLine("Use Case #2: {0}", solution.MinWindow("a", "aa"));
+            Console.WriteLine("Use Case #3: {0}", solution.MinWindow("ab", "aa"));
+            Console.WriteLine("Use Case #4: {0}", solution.MinWindow("aa", "aa"));
             Console.ReadLine();
         }
     }
 
     public class Solution
     {
-        private string GetWindow(string s, List<List<int>> candidates, List<int> pointers, out int minCandidateNdx)
+        private class Pointers
         {
-            var min = s.Length;
-            var max = -1;
-            minCandidateNdx = -1;
-            for (var i = 0; i < candidates.Count; i++)
-            {
-                var ndx = candidates[i][pointers[i]];
-                if (ndx < min)
-                {
-                    min = ndx;
-                    minCandidateNdx = i;
-                }
+            private string input;
 
-                if (ndx > max)
+            private List<List<int>> candidates;
+
+            private List<int> requiredCount;
+
+            private List<int> curPointers = new List<int>();
+
+            private int minPointerNdx;
+
+            private int minInputNdx;
+            private int maxInputNdx;
+
+            public Pointers(string input, List<List<int>> candidates, List<int> requiredCount)
+            {
+                this.input = input;
+                this.candidates = candidates;
+                this.requiredCount = requiredCount;
+                this.Initialize();
+            }
+
+            public bool IsValid { get; private set; }
+
+            public string CurrentResult
+            {
+                get
                 {
-                    max = ndx;
+                    if (!this.IsValid)
+                    {
+                        return string.Empty;
+                    }
+
+                    return this.input.Substring(this.minInputNdx, this.maxInputNdx - this.minInputNdx + 1);
                 }
             }
 
-            return s.Substring(min, max - min + 1);
+            public void Next()
+            {
+                this.curPointers[this.minPointerNdx]++;
+                this.FindBorders();
+            }
+
+            private void FindBorders()
+            {
+                this.minInputNdx = this.input.Length;
+                this.maxInputNdx = -1;
+
+                this.IsValid = true;
+
+                for (var i = 0; i < this.candidates.Count; i++)
+                {
+                    var iReq = this.requiredCount[i];
+                    var iCandidates = this.candidates[i];
+                    var iPointer = this.curPointers[i];
+                    if (iCandidates.Count < iPointer + iReq)
+                    {
+                        this.IsValid = false;
+                        return;
+                    }
+
+                    this.curPointers.Add(0);
+                    for (var j = iPointer; j < iPointer + iReq; j++)
+                    {
+                        if (iCandidates[j] < this.minInputNdx)
+                        {
+                            this.minInputNdx = iCandidates[j];
+                            this.minPointerNdx = i;
+                        }
+
+                        if (iCandidates[j] > this.maxInputNdx)
+                        {
+                            this.maxInputNdx = iCandidates[j];
+                        }
+                    }
+                }
+            }
+
+            private void Initialize()
+            {
+                for (var i = 0; i < this.candidates.Count; i++)
+                {
+                    this.curPointers.Add(0);
+                }
+
+                this.FindBorders();
+            }
         }
 
         public string MinWindow(string s, string t)
@@ -51,45 +120,52 @@ namespace MinWindowSubstring
                 return string.Empty;
             }
 
-            var map = new Dictionary<char, List<int>>();
+            #region Iterate through t to account its characters and count
+            var charMap = new Dictionary<char, List<int>>();
+            var countMap = new Dictionary<char, int>();
             foreach (var ch in t)
             {
-                if (!map.ContainsKey(ch))
+                if (!charMap.ContainsKey(ch))
                 {
-                    map.Add(ch, new List<int>());
+                    charMap.Add(ch, new List<int>());
                 }
-            }
 
+                if (!countMap.ContainsKey(ch))
+                {
+                    countMap.Add(ch, 0);
+                }
+
+                countMap[ch]++;
+            }
+            #endregion
+
+            #region iterate through input string to collect where characters from t are
             for (var i = 0; i < s.Length; i++)
             {
-                if (map.ContainsKey(s[i]))
+                if (charMap.ContainsKey(s[i]))
                 {
-                    map[s[i]].Add(i);
+                    charMap[s[i]].Add(i);
                 }
             }
+            #endregion
 
-            var candidates = map.Values.ToList();
-            if (candidates.Any(l => l.Count == 0))
+            var candidates = new List<List<int>>();
+            var requiredCount = new List<int>();
+            foreach (var kvp in charMap)
             {
-                return string.Empty;
+                candidates.Add(kvp.Value);
+                requiredCount.Add(countMap[kvp.Key]);
             }
 
-            var pointers = new List<int>();
-            for (var i = 0; i < candidates.Count; i++)
+            var pointers = new Pointers(s, candidates, requiredCount);
+            var result = "";
+            while (pointers.IsValid)
             {
-                pointers.Add(0);
-            }
-
-            int minCandidateNdx;
-            var result = this.GetWindow(s, candidates, pointers, out minCandidateNdx);
-            while (pointers[minCandidateNdx] + 1 < candidates[minCandidateNdx].Count)
-            {
-                pointers[minCandidateNdx]++;
-                var resCandidate = this.GetWindow(s, candidates, pointers, out minCandidateNdx);
-                if (resCandidate.Length < result.Length)
+                if (string.IsNullOrEmpty(result) || result.Length > pointers.CurrentResult.Length)
                 {
-                    result = resCandidate;
+                    result = pointers.CurrentResult;
                 }
+                pointers.Next();
             }
 
             return result;
