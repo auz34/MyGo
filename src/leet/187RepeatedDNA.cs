a1f95592701164832d12193784ca7577f1f21f9c
auz34 <andrewzavgorodniy@gmail.com>
2016-05-12 17:04:34 -0700

    repeated DNA problem

---
+++ src/leet/187RepeatedDNA.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepeatedDNA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            var dict = new Dictionary<string, int>();
+
+            for (var i = 0; i < s.Length - 10 + 1; i++)
+            {
+                var seq = s.Substring(i, 10);
+                if (dict.ContainsKey(seq))
+                {
+                    dict[seq]++;
+                }
+                else
+                {
+                    dict.Add(seq, 1);
+                }
+            }
+
+            return new List<string>(dict.Where(kvp => kvp.Value > 1).Select(kvp => kvp.Key));
+        }
+    }
+}
